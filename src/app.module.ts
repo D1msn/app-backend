203bdb8bb630e8fac23b5776c4be9ff5904b1a2f
D1msn <flash.volkov@gmail.com>
2022-07-13 23:55:03 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/users.model';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      port: +process.env.MYSQL_PORT,
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
